195de2a2674ffd1aa1de326424b15867f195fc7b
Dulas <abdullahtshabalala@gmail.com>
2025-05-09 18:59:25 +0200

    Update 106.js

--- 106.js
+++ 106.js
@@ -1,111 +1,52 @@
 const content = {
       series: [
-
-       
-
-
-
-//Series 
         {
           title: "Konosuba Season 1",
-          image: " 2.jpeg",
-
-          description: "The adventure of kazuma sato and his party",
-
+          image: "2.jpg",
+          description: "An intergalactic adventure across unknown planets.",
           episodes: [
-            { title: "Episode 1 ", link: " https://drive.google.com/file/d/110UyhRjYnSOomo3-9Wh30BZonjVHr7Nu/preview" },
-
-            { title: "Episode 2 ", link: "https://drive.google.com/file/d/1128cXGsLQ_wUcjls8hcZbfy-YbeRwChc/preview " },
-
-            { title: "Episode 3 ", link: "https://drive.google.com/file/d/110VFc93Wq49TvnsWq9AclCCsyt5scLSC/preview " },
-
-            { title: "Episode 4 ", link: "https://drive.google.com/file/d/119B_PFF3wtTyQMu3pu4-rBpN1rrfI7dR/preview " },
-
-            { title: "Episode 5  ", link: " https://drive.google.com/file/d/116FV6qv7Hjdpkj4v7deuoactP-ee-T9A/preview" },
-
-            { title: "Episode 6  ", link: "https://drive.google.com/file/d/11A96GHkRYHQ1uwRwa4eMiVBxbqM5YcmA/preview " },
-
-            { title: "Episode 7  ", link: "https://drive.google.com/file/d/11TiSpcFJr2N8r7GPyIGgmgy2tHfnQTtL/preview " },
-
-            { title: "Episode 8 ", link: "https://drive.google.com/file/d/11tAEoUZsUeQa4ja3-Nnt76nOKR2_w5wa/preview " },
-
-            { title: "Episode 9 ", link: "https://drive.google.com/file/d/11V8IleXjjcnlUAparkf44XBFEUwxK8Ou/preview " },
-
-            { title: "Episode 10 ", link: " https://drive.google.com/file/d/11uRZmLQxY52jeIsQKiw6vf2d1jTiCBlr/preview" },
-
+            { title: "Episode 1", link: "https://drive.google.com/file/d/110UyhRjYnSOomo3-9Wh30BZonjVHr7Nu/preview" },
+            { title: "Episode 2", link: "https://drive.google.com/file/d/1128cXGsLQ_wUcjls8hcZbfy-YbeRwChc/preview" },
+            { title: "Episode 3", link: "https://drive.google.com/file/d/110VFc93Wq49TvnsWq9AclCCsyt5scLSC/preview" },
+            { title: "Episode 4", link: "https://drive.google.com/file/d/119B_PFF3wtTyQMu3pu4-rBpN1rrfI7dR/preview" },
+            { title: "Episode 5", link: "https://drive.google.com/file/d/116FV6qv7Hjdpkj4v7deuoactP-ee-T9A/preview" },
+            { title: "Episode 6", link: "https://drive.google.com/file/d/11A96GHkRYHQ1uwRwa4eMiVBxbqM5YcmA/preview" },
+            { title: "Episode 7", link: "https://drive.google.com/file/d/11TiSpcFJr2N8r7GPyIGgmgy2tHfnQTtL/preview" },
+            { title: "Episode 8", link: "https://drive.google.com/file/d/11tAEoUZsUeQa4ja3-Nnt76nOKR2_w5wa/preview" },
+            { title: "Episode 9", link: "https://drive.google.com/file/d/11V8IleXjjcnlUAparkf44XBFEUwxK8Ou/preview" },
+            { title: "Episode 10", link: "https://drive.google.com/file/d/11uRZmLQxY52jeIsQKiw6vf2d1jTiCBlr/preview" }
           ]
-        },
+        }
+      ],
 
 
 
 
-
-
-]},
-    
-
-
-
-
-
-
-
-//Movies
-movies: [
-
-
-{
-          title: "Captain America:Brave new world",
-          image: "c.jpeg",
-
-          description: "The rise of a new captain amarica ",
-
-          episodes: [
-            { title: "watch now", link:" https://drive.google.com/file/d/13ZnENWshn2eFwju7D0VQP8mmbxp0o1Xo/preview " },
-          ]
-        },
-
-
+      movies: [
         {
           title: "Your Name",
-          image: "1.jpeg",
-
-          description: "",
-
-          episodes: [
-            { title: "watch now", link:" https://drive.google.com/file/d/10e007gPw7-zykD4XnHU_v_HV3mS7Y72Y/preview" },
-
-          ]
-        },
-
-
+          image:"1.jpeg",
+          description: "A beautiful romantic fantasy that transcends time and space.",
+          link: "https://drive.google.com/file/d/10e007gPw7-zykD4XnHU_v_HV3mS7Y72Y/preview"
+        }
 
 
 
 {
           title: "The Life List",
-          image: "ll.jpeg",
-
-          description: " After  her mother's passing Alex is forced to complete a list she compiled when she was 13 only so she could have some of her mother's inherentence ,but on her Journey she finds secrets about herself. ",
-
-          episodes: [
-            { title: "watch now", link:"https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview  " },
-
-          ]
-        },
-
+          image:"11.jpeg",
+                    description: " After  her mother's passing Alex is forced to complete a list she compiled when she was 13 only so she could have some of her mother's inherentence ,but on her Journey she finds secrets about herself. ",
+       link:"https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview  "
+   
+        }
 
 
 {
           title: "Power Rangers",
-          image: "pr.jpeg",
-
-          description: "",
-
-          episodes: [
-            { title: "watch now", link:"https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview" },
-          ]
-        },
+          image:"pr.jpeg",
+          description: " .",
+            link:"https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview" 
+        }
 
 
 
@@ -113,4 +54,9 @@ movies: [
 
 
 
-      ]};
+
+
+
+
+      ]
+    };
