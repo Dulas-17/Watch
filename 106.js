7ba41b9b9073f189b03962bc9717280b67028994
Dulas <abdullahtshabalala@gmail.com>
2025-06-02 16:39:12 +0200

    Update 106.js

--- 106.js
+++ 106.js
@@ -1,90 +1,62 @@
 const content = {
-      series: [
-
-               {
-title: "Bleach:TYBW P2",
-image: "Images/cour2.jpeg",
-description: "When a new enemy appears, Ichigo Kurosaki jumps back into the battlefield with his Zanpakuto to help those in need. ",
- episodes: [
-                   { title: "Episode 1", link:"https://drive.google.com/file/d/10D0xtKf_CPz5ODJAiX2A5wfyCg57NcIg/preview" },
-                   { title: "Episode 2",  link:"https://drive.google.com/file/d/1-K6_2qaz1ZCz7u54FSvNX-hPq3qbqmZf/preview" },
-                   { title: "Episode 3",                  link:"https://drive.google.com/file/d/1-TQNzstys6szmuu-5iPmnxmZ6-oznFc2/preview" },
-
-
-
-                     
-   
-]},
-
-
-
-        {
-          title: "Konosuba (s1)",
-          image: "Images/Cover4.jpg",
-          description: "After a traffic accident, Kazuma Sato's disappointingly brief life was supposed to be over, but a goddess gives him a second chance.",
-          episodes: [
-            { title: "Episode 1", link: "https://drive.google.com/file/d/110UyhRjYnSOomo3-9Wh30BZonjVHr7Nu/preview" },
-            { title: "Episode 2", link: "https://drive.google.com/file/d/1128cXGsLQ_wUcjls8hcZbfy-YbeRwChc/preview" },
-            { title: "Episode 3", link: "https://drive.google.com/file/d/110VFc93Wq49TvnsWq9AclCCsyt5scLSC/preview" },
-            { title: "Episode 4", link: "https://drive.google.com/file/d/119B_PFF3wtTyQMu3pu4-rBpN1rrfI7dR/preview" },
-            { title: "Episode 5", link: "https://drive.google.com/file/d/116FV6qv7Hjdpkj4v7deuoactP-ee-T9A/preview" },
-            { title: "Episode 6", link: "https://drive.google.com/file/d/11A96GHkRYHQ1uwRwa4eMiVBxbqM5YcmA/preview" },
-            { title: "Episode 7", link: "https://drive.google.com/file/d/11TiSpcFJr2N8r7GPyIGgmgy2tHfnQTtL/preview" },
-            { title: "Episode 8", link: "https://drive.google.com/file/d/11tAEoUZsUeQa4ja3-Nnt76nOKR2_w5wa/preview" },
-            { title: "Episode 9", link: "https://drive.google.com/file/d/11V8IleXjjcnlUAparkf44XBFEUwxK8Ou/preview" },
-            { title: "Episode 10", link: "https://drive.google.com/file/d/11uRZmLQxY52jeIsQKiw6vf2d1jTiCBlr/preview" },
-          ]
-        },
-
-
-
-      ],
-
-
-
-
-
-
-
-
-
-
-
-      movies: [
-        {
-          title: "Your Name(sub)",
-          image:"Images/Cover3.jpeg",
-          description: "Two teenagers share a profound, magical connection upon discovering they are swapping bodies. Things manage to become even more complicated when the boy and girl decide to meet in person.",
-          link: "https://drive.google.com/file/d/10e007gPw7-zykD4XnHU_v_HV3mS7Y72Y/preview"
-        }, 
-
-
-
-{
-          title: "The Life List",
-          image:"Images/Cover1.jpeg",
-                    description: "When her mother sends her on a quest to complete a teenage bucket list, a young woman uncovers family secrets, finds romance and rediscovers herself.",
-       link:"https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview  "
-   
-        },
-
-
-{
-          title: "Power Rangers",
-          image:"Images/Cover2.jpeg",
-          description: "A group of high-school students acquire superpowers after they discover an alien spaceship. They soon learn about its origin and use their abilities to fight against evil extraterrestrials.",
-            link:"https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview" 
-        },
-
-
-
-
-
-
-
-
-
-
-
+  series: [
+    {
+      title: "Bleach:TYBW P2",
+      image: "Images/cour2.jpeg",
+      description: "A battle for the soul of the universe continues.",
+      genres: ["Action", "Fantasy", "Anime","Shonen"], // Added genres
+      episodes: [
+        { title: "Episode 1", link: "https://drive.google.com/file/d/10D0xtKf_CPz5ODJAiX2A5wfyCg57NcIg/preview" },
+        { title: "Episode 2", link: "https://drive.google.com/file/d/1-K6_2qaz1ZCz7u54FSvNX-hPq3qbqmZf/preview" },
+        { title: "Episode 3", link: "https://drive.google.com/file/d/1-TQNzstys6szmuu-5iPmnxmZ6-oznFc2/preview" },
       ]
-    };
+    },
+    {
+      title: "Konosuba (s1)",
+      image: "Images/Cover4.jpg",
+      description: "An intergalactic adventure across unknown planets.",
+      genres: ["Comedy", "Fantasy", "Isekai", "Anime"], // Added genres
+      episodes: [
+        { title: "Episode 1", link: "https://drive.google.com/file/d/110UyhRjYnSOomo3-9Wh30BZonjVHr7Nu/preview" },
+        { title: "Episode 2", link: "https://drive.google.com/file/d/1128cXGsLQ_wUcjls8hcZbfy-YbeRwChc/preview" },
+        { title: "Episode 3", link: "https://drive.google.com/file/d/110VFc93Wq49TvnsWq9AclCCsyt5scLSC/preview" },
+        { title: "Episode 4", link: "https://drive.google.com/file/d/119B_PFF3wtTyQMu3pu4-rBpN1rrfI7dR/preview" },
+        { title: "Episode 5", link: "https://drive.google.com/file/d/116FV6qv7Hjdpkj4v7deuoactP-ee-T9A/preview" },
+        { title: "Episode 6", link: "https://drive.google.com/file/d/11A96GHkRYHQ1uwRwa4eMiVBxbqM5YcmA/preview" },
+        { title: "Episode 7", link: "https://drive.google.com/file/d/11TiSpcFJr2N8r7GPyIGgmgy2tHfnQTtL/preview" },
+        { title: "Episode 8", link: "https://drive.google.com/file/d/11tAEoUZsUeQa4ja3-Nnt76nOKR2_w5wa/preview" },
+        { title: "Episode 9", link: "https://drive.google.com/file/d/11V8IleXjjcnlUAparkf44XBFEUwxK8Ou/preview" },
+        { title: "Episode 10", link: "https://drive.google.com/file/d/11uRZmLQxY52jeIsQKiw6vf2d1jTiCBlr/preview" },
+      ]
+    },
+  ],
+
+
+
+
+  movies: [
+    {
+      title: "Your Name(sub)",
+      image: "Images/Cover3.jpeg",
+      description: "A beautiful romantic fantasy that transcends time and space.",
+      genres: ["Animation", "Romance", "Fantasy", "Drama","Anime"], // Added genres
+      link: "https://drive.google.com/file/d/10e007gPw7-zykD4XnHU_v_HV3mS7Y72Y/preview"
+    },
+
+    {
+      title: "The Life List",
+      image: "Images/Cover1.jpeg",
+      description: "After her mother's passing Alex is forced to complete a list she compiled when she was 13 only so she could have some of her mother's inheritance, but on her Journey she finds secrets about herself.",
+      genres: ["Drama", "Family", "Adventure","Romance"], // Added genres
+      link: "https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview"
+    },
+
+    {
+      title: "Power Rangers",
+      image: "Images/Cover2.jpeg",
+      description: "Five ordinary teens must become something extraordinary when they learn that their small town of Angel Grove – and the world – is about to be annihilated by an alien threat. Chosen by destiny, our heroes quickly discover they are the only ones who can save the planet. But to do so, they will have to overcome their real-world issues and band together as the Power Rangers before it’s too late.",
+      genres: ["Action", "Sci-Fi", "Adventure"], // Added genres
+      link: "https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview"
+    },
+  ]
+};
